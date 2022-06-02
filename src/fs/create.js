7bf43033e4bd5385c1e9ae56a50d9905b1fac8b7
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-03 00:07:23 +0300

    feat: add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,22 @@
+import fs from "fs";
+import path from "path";
+
 export const create = async () => {
-    // Write your code here 
+  
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, 'src/fs/files', 'fresh.txt');
+
+  fs.access(pathFile, fs.F_OK, (err) => {
+    if(err) {
+      fs.writeFile(pathFile, "I am fresh and young", (err) => {   
+        if(err) {
+          return console.log(err);
+        }
+      }); 
+      return
+    }
+    console.log('FS operation failed')
+  })
 };
+
+create();
