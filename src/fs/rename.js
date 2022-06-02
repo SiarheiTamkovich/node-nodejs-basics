2c3f71f8d14a659bd0f0d918218dc2a45b22ddd0
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-03 01:46:19 +0300

    feat: add fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,24 @@
+import fs from "fs";
+import path from "path";
+
 export const rename = async () => {
-    // Write your code here 
+
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, 'src/fs/files/wrongFilename.txt');
+  const pathFileNew = path.join(__dirname, 'src/fs/files/properFilename.md');
+
+  fs.access(pathFileNew, fs.F_OK, (err) => {
+    if(err) {
+      fs.access(pathFile, fs.F_OK, (err) => {
+        if(err) {
+          return console.log('FS operation failed');
+        }
+        return fs.rename(pathFile, pathFileNew, () => {});
+      })
+      return;
+    }
+    return console.log('FS operation failed');
+  })
 };
+rename();
+
