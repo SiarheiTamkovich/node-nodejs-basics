3f6730a971572971741e2bc913b62699bacf70cd
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-03 01:00:51 +0300

    feat: add copy folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,33 @@
+import fs from "fs";
+import path from "path";
+
 export const copy = async () => {
-    // Write your code here 
+
+  const __dirname = path.resolve();
+  const pathFolder = path.join(__dirname, 'src/fs/files');
+  const pathFolderNew = path.join(__dirname, 'src/fs/files_copy');
+
+  fs.access(pathFolder, fs.F_OK, (err) => {
+    if(err){
+      return console.log('FS operation failed');
+    }
+    fs.access(pathFolderNew, fs.F_OK, (err) => {
+      if(err){
+        fs.promises.mkdir(pathFolderNew, { recursive: true });
+        fs.readdir(pathFolder, (err, files) => {
+          files.forEach(file => {
+            fs.copyFile(`${pathFolder}/${file}`, `${pathFolderNew}/${file}`,(err) => {
+              if (err) {
+                  console.error(err)
+                  return
+              };
+            })
+          })
+        })
+        return;
+      }
+      return console.log('FS operation failed');
+    })
+  })
 };
+copy();
