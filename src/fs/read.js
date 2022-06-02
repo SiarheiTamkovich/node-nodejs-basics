4b32f8122938c9d0aa3ccab6329cb76631856518
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-03 02:32:09 +0300

    feat: add fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -3,5 +3,13 @@ import path from "path";
 
 export const read = async () => {
 
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, 'src/fs/files/fileToRead.txt');
+
+  fs.access(pathFile, fs.F_OK, (err)=> {
+    if(err) return console.log('FS operation failed');
+    const $stream = fs.createReadStream(pathFile);
+    $stream.on('data', (data) => console.log(data.toString()));
+  });
 };
 read();
