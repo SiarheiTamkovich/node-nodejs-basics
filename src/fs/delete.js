7bbd69f8c8aa780242857e4c21bdff816f2d6b15
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-03 01:59:38 +0300

    feat; add fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import fs from "fs";
+import path from "path";
+
 export const remove = async () => {
-    // Write your code here 
+  
+  const __dirname = path.resolve();
+  const pathFile = path.join(__dirname, 'src/fs/files/fileToRemove.txt');
+
+  fs.access(pathFile, fs.F_OK, (err) => {
+    if(err) return console.log('FS operation failed');
+    fs.unlink(pathFile, () => {});
+  })
 };
+remove();
