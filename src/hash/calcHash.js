b5686cddcdf49f0a4295b71158954332ed599f42
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-04 23:28:11 +0300

    feat: add task hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { createHash } from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const hash = (str) => {
+    return createHash('sha256').update(str).digest('hex');
+  }
+  
+  const readableStream = fs.createReadStream(
+    path.join(__dirname, './files/fileToCalculateHashFor.txt'), 'utf-8'
+  );
+  readableStream.on('data', chunk => console.log(hash(chunk)));
 };
+calculateHash();
+
+// for Powershell terminal run commands:
+// node src/hash/calcHash
