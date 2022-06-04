161ef822f541ddba4cc19ee14efa46619765b4f2
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-04 22:37:20 +0300

    feat: add task streams/tranform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,15 @@
 export const transform = async () => {
-    // Write your code here 
+
+  const { stdin, stdout } = process;
+
+  stdout.write('Enter text:\n');
+  stdin.on('data', data => {
+    const reverseData = data.toString().split('').reverse().join('');
+    stdout.write(`Reverse: ${reverseData}`);
+    process.exit();
+  });
 };
+transform();
+
+// for Powershell terminal run commands:
+// node src/streams/transform
