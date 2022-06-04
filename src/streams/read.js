2f45decbc921f00996b9bf6091518ada818cddf9
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-04 19:43:23 +0300

    feat: add task streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const readableStream = fs.createReadStream(
+    path.join(__dirname, './files/fileToRead.txt'), 'utf-8'
+  );
+  readableStream.on('data', chunk => console.log('\x1b[36m%s\x1b[0m', chunk))
 };
+read();
+
+// for Powershell terminal run commands:
+// node src/streams/read
