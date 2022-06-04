73cda754d48670e4af3a19c1c331eb8b8841cc5d
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-05 02:18:25 +0300

    feat: add task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,21 @@
+import childProcess from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const child = childProcess.fork(__dirname + '/files/script', args);
+
+  process.stdin.on('data', (chunk) => {
+    if (chunk.toString().includes('CLOSE')) process.exit(0);
+    process.stdout.write(`Received from child process: ${chunk.toString()}\n`)
+  });
+  
 };
+spawnChildProcess(['args1', 'args2', 'args3', 'args4']);
+
+// for Powershell terminal run commands:
+// node src/cp/cp
