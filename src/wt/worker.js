5b26dfb81a4ab50fb5558b60cd3ec91bf11a274b
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-05 16:36:03 +0300

    feat: add task wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,15 @@
+import {parentPort, workerData} from 'worker_threads'
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
+    
+    const message = {
+        status: 'resolved',
+        data: nthFibonacci(workerData.startNumber),
+    }
+    parentPort.postMessage(message);
     // This function sends result of nthFibonacci computations to main thread
 };
+sendResult();
