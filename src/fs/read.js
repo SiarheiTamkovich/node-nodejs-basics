81cb74f743998f014220e89d55c888e44929f923
SiarheiTamkovich <mpg2000@mail.ru>
2022-06-05 17:04:31 +0300

    refactor: delete comments

--- src/fs/read.js
+++ src/fs/read.js
@@ -8,8 +8,6 @@ export const read = async () => {
 
   fs.access(pathFile, fs.F_OK, (err)=> {
     if(err) return console.error('\x1b[31m%s\x1b[0m','FS operation failed');
-    // const stream = fs.createReadStream(pathFile);
-    // stream.on('data', (data) => console.log(data.toString()));
     fs.readFile(pathFile, 'utf8', (err, data) => console.log('\x1b[36m%s\x1b[0m', data));
   });
 };
